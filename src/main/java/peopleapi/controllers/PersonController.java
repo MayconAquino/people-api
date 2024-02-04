eb9ae0493a27e1ad86df7c68905015c2f013870a
CIANDT\mayconaquino <mayconaquino@ciandt.com>
2024-02-04 14:17:08 -0300

    Reiniciando novamente devidos problemas locais

--- src/main/java/peopleapi/controllers/PersonController.java
+++ src/main/java/peopleapi/controllers/PersonController.java
@@ -1,27 +1,18 @@
 package peopleapi.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import peopleapi.model.Person;
-import peopleapi.serviceImpl.PersonServiceImpl;
-
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/persons")
 public class PersonController {
 
-    @Autowired
-    private PersonServiceImpl personServiceImpl;
-
 
     @GetMapping
-    public ResponseEntity<List<Person>> getAllPersons() {
-        return ResponseEntity.status(HttpStatus.OK).body(personServiceImpl.getAllPersons());
+    public ResponseEntity<String> testeRetorno() {
+        return ResponseEntity.ok("Retorno feito");
     }
 }
