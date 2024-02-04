068978fc283618ccc7c48cdb17f65c245f377132
CIANDT\mayconaquino <mayconaquino@ciandt.com>
2024-02-04 15:41:44 -0300

    Criando conexão com banco e migration

--- src/main/java/peopleapi/model/Person.java
+++ src/main/java/peopleapi/model/Person.java
@@ -1,11 +1,20 @@
 package peopleapi.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Person {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String cpf;
     private String address;
